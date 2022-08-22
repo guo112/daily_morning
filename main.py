f4982d7c9b2d489f873e9b21aa20f9c334881411
guo112 <73544337+guo112@users.noreply.github.com>
2022-08-23 01:25:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature,low,high= get_weather()
-# weather, temperature,low,high,humidity,wind,pm25= get_weather()
+# wea, temperature,low,high= get_weather()
+wea, temperature,low,high,humidity,wind,pm25= get_weather()
 # data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 data = {"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"max_temperature": {"value":high,"color":get_random_color()},"humidity": {"value":humidity,"color":get_random_color()},"wind": {"value":wind,"color":get_random_color()},"pm25": {"value":pm25,"color":get_random_color()},"min_temperature":{"value":low, "color":get_random_color()}}
 # # res = wm.send_template(user_id, template_id, data)
