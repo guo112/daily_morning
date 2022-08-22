fc4a9bcf8f66c64db10f37c27488c0f1af798f12
guo112 <73544337+guo112@users.noreply.github.com>
2022-08-23 01:03:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][1]
-  return weather['weather'], math.floor(weather['temp'])
+  weather = res['data']['list'][0]
+  return weather['weather'], math.floor(weather['temp']),weather['humidity'],weather['wind'],weather['low'],weather['high']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -47,10 +47,10 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-# weather, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-# data = {"weather":{"value":weather,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"max_temperature": {"value":max_temperature,"color":get_random_color()},"min_temperature":{"value":min_temperature, "color":get_random_color()}}
+wea, temperature,low,high= get_weather()
+# weather, temperature,low,high= get_weather()
+# data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":weather,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"max_temperature": {"value":high,"color":get_random_color()},"min_temperature":{"value":low, "color":get_random_color()}}
 # # res = wm.send_template(user_id, template_id, data)
 res = wm.send_template(user_id, template_id, data)
 print(res)
